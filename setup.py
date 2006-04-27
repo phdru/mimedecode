1571360154c37f7c8d59d8c49876770f511af8c8
Oleg Broytman <phd@phdru.name>
2006-04-27 14:53:24 +0000

    Version 2.1.0.
    git-svn-id: file:///home/phd/archive/SVN/mimedecode@6 a778b35f-2f12-0410-a9f6-9b1356d76ba6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 
 from distutils.core import setup
-from mimedecode import __version__ as version
+from mimedecode import _version, __copyright__
 
 
 setup(name = "mimedecode",
-   version = version + 'd',
+   version = _version,
    description = "BroytMann mimedecode.py",
-   long_description = "BroytMann mimedecode.py, Copyright (C) 2001-2004 PhiloSoft Design",
+   long_description = "BroytMann mimedecode.py, " + __copyright__,
    author = "Oleg Broytmann",
    author_email = "phd@phd.pp.ru",
    url = "http://phd.pp.ru/Software/Python/#mimedecode",
