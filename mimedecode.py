54430444fc747add2c4e10e17f61d434cb0dbe6f
Oleg Broytman <phd@phdru.name>
2013-12-11 00:58:16 +0400

    Version 2.1.4: add installation instruction in INSTALL.txt

--- mimedecode.py
+++ mimedecode.py
@@ -2,7 +2,7 @@
 """Decode MIME message"""
 
 
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 __author__ = "Oleg Broytman <phd@phdru.name>"
 __copyright__ = "Copyright (C) 2001-2013 PhiloSoft Design"
 __license__ = "GNU GPL"
