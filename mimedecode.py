fa51f1e417da0f8d66649ebe67c78a58b1b88eae
Oleg Broytman <phd@phdru.name>
2014-02-26 20:11:32 +0400

    Version 2.3.8
    Add option --remove-params=header to remove all parameters from the header.

--- mimedecode.py
+++ mimedecode.py
@@ -18,7 +18,7 @@ Broytman mimedecode.py version %s, %s
 def usage(code=0, errormsg=''):
     version(0)
     sys.stdout.write("""\
-Usage: %s [-h|--help] [-V|--version] [-cCDP] [-H|--host=hostname] [-f charset] [-d header] [-p header:param] [-r header] [-R header:param] [-beit mask] [-o output_file] [input_file [output_file]]
+Usage: %s [-h|--help] [-V|--version] [-cCDP] [-H|--host=hostname] [-f charset] [-d header] [-p header:param] [-r header] [-R header:param] [--remove-params=header] [-beit mask] [-o output_file] [input_file [output_file]]
 """ % me)
     if errormsg:
         sys.stderr.write(errormsg + '\n')
@@ -99,6 +99,15 @@ def decode_headers(msg):
 
     for header in gopts.remove_headers:
         del msg[header]
+
+    for header in gopts.remove_all_params:
+        value = msg[header]
+        if value is None:
+            continue
+        if ';' not in value:
+            continue
+        del msg[header]
+        msg[header] = value.split(';')[0].strip()
 
     for header, param in gopts.remove_header_params:
         msg.del_param(param, header)
@@ -291,6 +300,8 @@ class GlobalOptions:
     remove_headers = []
     # A list of headers parameters to remove
     remove_header_params = []
+    # A list of headers to be stripped of all parameters
+    remove_all_params = []
 
     totext_mask = [] # A list of content-types to decode
     binary_mask = [] # A list to pass through
@@ -309,7 +320,7 @@ def get_opt():
     try:
         options, arguments = getopt(sys.argv[1:],
             'hVcCDPH:f:d:p:r:R:b:e:i:t:o:',
-            ['help', 'version', 'host'])
+            ['help', 'version', 'host=', 'remove-params='])
     except GetoptError:
         usage(1)
 
@@ -338,6 +349,8 @@ def get_opt():
             gopts.remove_headers.append(value)
         elif option == '-R':
             gopts.remove_header_params.append(value.split(':', 1))
+        elif option == '--remove-params':
+            gopts.remove_all_params.append(value)
         elif option == '-t':
             gopts.totext_mask.append(value)
         elif option == '-b':
