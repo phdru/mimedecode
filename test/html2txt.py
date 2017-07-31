6dffe39a733fb41e662d92124914398d16ed44d2
Oleg Broytman <phd@phdru.name>
2017-08-01 00:46:01 +0300

    Feat(tests): Use tox for testing

--- test/html2txt.py
+++ test/html2txt.py
@@ -4,11 +4,21 @@ from __future__ import print_function
 import sys
 from m_lib.net.www.html import HTMLFilter
 
-with open(sys.argv[1], 'r') as f:
-    html = f.read()
+PY2 = sys.version_info[0] < 3
+if PY2:
+    with open(sys.argv[1], 'r') as f:
+        html = f.read()
+else:
+    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+        html = f.read()
 
 filter = HTMLFilter()
 filter.feed(html)
 filter.close()
 
-print(filter.accumulator)
+if PY2:
+    print(filter.accumulator)
+else:
+    if not isinstance(filter.accumulator, bytes):
+        filter.accumulator = filter.accumulator.encode('utf-8')
+    sys.stdout.buffer.write(filter.accumulator + b'\n')
