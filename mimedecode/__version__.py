c5e23121c09605e62c32c01fd24acf0e1afc02d6
Oleg Broytman <phd@phdru.name>
2018-03-28 21:46:13 +0300

    Docs: 2018 year
    [skip ci]

--- mimedecode/__version__.py
+++ mimedecode/__version__.py
@@ -1,4 +1,4 @@
 __version__ = "2.9.0"
 __author__ = "Oleg Broytman <phd@phdru.name>"
-__copyright__ = "Copyright (C) 2001-2017 PhiloSoft Design"
+__copyright__ = "Copyright (C) 2001-2018 PhiloSoft Design"
 __license__ = "GNU GPL"
