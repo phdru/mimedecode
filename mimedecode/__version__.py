a8c62643109b7d1351cb066730b88965fb3ad843
Oleg Broytman <phd@phdru.name>
2022-12-07 17:44:19 +0300

    CI(GHActions): Set `$LD_LIBRARY_PATH`
    Python 2.7 on Linux requires `$LD_LIBRARY_PATH`.

--- mimedecode/__version__.py
+++ mimedecode/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "3.1.0.post3"
+__version__ = "3.1.0.post4"
 __author__ = "Oleg Broytman <phd@phdru.name>"
 __copyright__ = "Copyright (C) 2001-2022 PhiloSoft Design"
 __license__ = "GNU GPL"
