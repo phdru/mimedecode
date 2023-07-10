45c83a7df58192ecc6c639e967b67422c0d0cd78
Oleg Broytman <phd@phdru.name>
2023-07-10 19:23:31 +0300

    Docs: Version 3.1.0.post5

--- docs/mimedecode.rst
+++ docs/mimedecode.rst
@@ -334,7 +334,7 @@ Oleg Broytman <phd@phdru.name>
 COPYRIGHT
 ---------
 
-Copyright (C) 2001-2022 PhiloSoft Design.
+Copyright (C) 2001-2023 PhiloSoft Design.
 
 LICENSE
 -------
--- mimedecode/__version__.py
+++ mimedecode/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "3.1.0.post4"
+__version__ = "3.1.0.post5"
 __author__ = "Oleg Broytman <phd@phdru.name>"
-__copyright__ = "Copyright (C) 2001-2022 PhiloSoft Design"
+__copyright__ = "Copyright (C) 2001-2023 PhiloSoft Design"
 __license__ = "GNU GPL"
