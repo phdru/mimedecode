c7205de2f10dfa0064b02e32bd811a377c1700c3
Oleg Broytman <phd@phdru.name>
2023-09-17 18:39:54 +0300

    CI(pip): Ensure `pip` only if needed
    This is to work around a problem in conda with Python 3.7 -
    it brings in wrong version of `setuptools` incompatible with Python 3.7.

--- mimedecode/__version__.py
+++ mimedecode/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "3.1.0.post5"
+__version__ = "3.1.0.post6"
 __author__ = "Oleg Broytman <phd@phdru.name>"
 __copyright__ = "Copyright (C) 2001-2023 PhiloSoft Design"
 __license__ = "GNU GPL"
