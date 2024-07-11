1ce574e0c45914ba8d7ae3c3ebeae418b2ade226
Oleg Broytman <phd@phdru.name>
2024-07-11 21:31:46 +0300

    Version 3.2.0.post1: CI(GHActions): Switch to `setup-miniconda`

--- mimedecode/__version__.py
+++ mimedecode/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "3.2.0"
+__version__ = "3.2.0.post1"
 __author__ = "Oleg Broytman <phd@phdru.name>"
 __copyright__ = "Copyright (C) 2001-2024 PhiloSoft Design"
 __license__ = "GNU GPL"
